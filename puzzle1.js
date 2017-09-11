8e1af137e6b1ee5af0060f15f702d97ff4e3cc9a
ngkerry <31704416+ngkerry@users.noreply.github.com>
2017-09-10 20:13:04 -0700

    Create puzzle1.js

---
+++ puzzle1.js
@@ -0,0 +1,161 @@
+Puzzle 1 
+
+if steps < 7 
+Down 
+Right
+Right
+Right
+Right
+Right 
+Up 
+Up 
+Right 
+Right 
+
+Puzzle 2
+If steps < 6 
+Down 
+Right
+Right
+Right
+Up
+Up
+Right
+Right
+Down 
+Down 
+Down 
+
+Puzzle 3 
+Down 
+Down 
+GetColor
+Save
+Down 
+If steps < 3
+right 
+right
+SetColor
+Right 
+Right 
+Up 
+
+Puzzle 4
+If steps < 4 
+Down 
+Right
+if getColor is blue 
+save 
+down 
+SetColor  
+Right 
+Right
+If getColor is Green 
+Save 
+Down
+SetColor
+right 
+right
+If getColor is Yellow
+Save 
+Down 
+SetColor 
+Right 
+Right 
+If getColor is Red 
+Save 
+Down 
+SetColor 
+Right 
+Right 
+Up 
+Right 
+Remember Get Color
+
+Puzzle 5 
+Right 
+Right 
+GetColor 
+Save 
+If getColor is Blue 
+Down 
+Right 
+Right 
+If getColor is Blue 
+Down 
+Right
+Right
+Else
+Up 
+Right 
+Right
+Right
+
+Puzzle 6  
+Right 
+If getColor is red
+Up 
+Up 
+Up 
+Direction = 0 
+Else 
+Down 
+Down 
+Down 
+Direction = 1
+if getColor is red
+left 
+if getColor is blue 
+right 
+if Direction = 0 
+Up 
+Else 
+Down 
+
+Puzzle 7
+Repeat if getColor isn’t Red
+Down
+If getColor is Red
+Save
+Right 
+Right 
+Repeat if getColor isn’t Red
+Up 
+SetColor 
+Right 
+Right 
+Repeat if getColor isn’t Red
+Down
+SetColor 
+Right 
+Right 
+Repeat if getColor isn’t Red 
+Up
+SetColor
+Right 
+Right
+Repeat if getColor isn’t Red
+Down 
+SetColor
+Right 
+
+Puzzle 8 
+If Steps < 8 
+Down 
+Right 
+Right 
+If Steps < 7 
+Up  
+Right 
+Right 
+If Steps < 7 
+Down 
+Right 
+Right 
+If Steps < 7 
+Up 
+Right 
+Right 
+If Steps < 7
+Down 
+Right 
